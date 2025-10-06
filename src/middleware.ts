4b104e0365757da123cfed17b882c0c1dfe3b3cb
piyush0049 <piyushjoshi81204@gmail.com>
2025-10-06 22:29:38 +0530

    changes

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,6 @@ import { NextRequest, NextResponse } from "next/server";
 export function middleware(req: NextRequest) {
   const token = req.cookies.get("token")?.value;
 
-  // If user is logged in, prevent access to login/signup pages
   if (token && ["/login", "/signup", "/"].includes(req.nextUrl.pathname)) {
     return NextResponse.redirect(new URL("/dashboard", req.url));
   }
